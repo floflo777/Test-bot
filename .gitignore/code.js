abd644b9b0f65b770772a687359dcdcaf15d3720
floflo777 <50406817+floflo777@users.noreply.github.com>
2020-07-15 11:22:42 +0200

    Update code.js

--- .gitignore/code.js
+++ .gitignore/code.js
@@ -1,10 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const datas = require("./datas.json");
+client.datas = datas
+
+const fs = require("fs");
+const path = require("path");
+
 client.login(process.env.TOKEN);
+console.log("Test-bot is on");
 
 client.on("message", message => {
   if (message.content.startsWith("!test")) {
     message.channel.send("ok");
+    fs.appendFile('./datas.json', JSON.stringify("donnees"), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Test-bot datas file is updated.");
+    });
   }
 });
